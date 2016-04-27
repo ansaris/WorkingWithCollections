1df95370e3136df14cb9032458e91fed337a31d2
ansaris <salman.ansari.usa@gmail.com>
2016-04-27 16:38:53 -0500

    Create Car and Book classes.

--- WorkingWithCollections/Program.cs
+++ WorkingWithCollections/Program.cs
@@ -12,4 +12,17 @@ namespace WorkingWithCollections
         {
         }
     }
+
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+    }
 }
